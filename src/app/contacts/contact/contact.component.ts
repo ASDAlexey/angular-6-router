7fbea1a7e05b4b3a73a326db6d1c99875af22277
Alexey <asdalexey@yandex.ru>
2018-05-29 23:36:54 +0300

    #11 optimise map

--- src/app/contacts/contact/contact.component.ts
+++ src/app/contacts/contact/contact.component.ts
@@ -15,8 +15,8 @@ export class ContactComponent {
   constructor(private route: ActivatedRoute, private http: HttpClient) {
     const api = 'http://jsonplaceholder.typicode.com/';
     route.params.pipe(
-      map((p: any) => p.id),
-      switchMap((id: number) => (http.get(`${api}users/${id}`))),
+      // map((p: any) => p.id),
+      switchMap((p: any) => (http.get(`${api}users/${p.id}`))),
       map((contact: any) => Object.assign({}, contact, { website: api + contact.website })),
       // startWith({ name: 'Loading', website: 'Loading' }),
     ).subscribe(this.user$);
